b818eb8f6d1f6632411bc2cf4789334705ec696f
Shifter447 <tbesdemiotis@gmail.com>
2025-09-02 10:05:04 +0200

    Bird mechanic

---
+++ gamejam/Gamejam/Assets/scripts/Bird.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed = 6f;                 // snelheid van de vogel
+    private Vector2 direction;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        // Kies random of de vogel van links of rechts komt
+        bool fromLeft = Random.value > 0.5f;
+
+        if (fromLeft)
+        {
+            transform.position = new Vector2(-10f, transform.position.y); // start links
+            direction = Vector2.right;
+            spriteRenderer.flipX = false; // kijkt naar rechts
+        }
+        else
+        {
+            transform.position = new Vector2(10f, transform.position.y);  // start rechts
+            direction = Vector2.left;
+            spriteRenderer.flipX = true;  // kijkt naar links
+        }
+    }
+
+    void Update()
+    {
+        // Beweeg de vogel
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        // Verwijder als vogel buiten beeld is
+        if (Mathf.Abs(transform.position.x) > 15f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject); // speler verwijderen, later GameManager triggeren
+        }
+    }
+}
---
+++ gamejam/Gamejam/Assets/scripts/BirdSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour
+{
+    public GameObject birdPrefab;
+    public float spawnInterval = 2f;
+    public float[] lanes = { 2f, 4f, 6f, 8f }; // Y-posities (paden)
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnBird), 1f, spawnInterval);
+    }
+
+    void SpawnBird()
+    {
+        float laneY = lanes[Random.Range(0, lanes.Length)];
+        Vector2 spawnPos = new Vector2(0f, laneY); // X wordt in Bird.cs gezet
+        Instantiate(birdPrefab, spawnPos, Quaternion.identity);
+    }
+}
